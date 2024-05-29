74096d0e7c0dd9768749b7e5f2291800b92cb8c5
Christopher <c.goetze@outlook.com>
2024-05-29 19:22:18 +0200

    Added example prompts

---
+++ examples.py
@@ -0,0 +1,16 @@
+
+PROMPT_EXAMPLES = [
+    "What is your name?",
+    "What is the weather like in Frankfurt am Main?",
+    "What is the current temperature in London?",
+    "Does it currently rain in New York?",
+    "What is the weather forecast for tomorrow in Paris?",
+    "Is it cloudy or sunny in Los Angeles?",
+    "What is the wind speed in Tokyo?",
+    "How is the weather going to develop over the next few days in Berlin?",
+    "How are rainbows formed?",
+    "Explain the concept of a thunderstorm.",
+    "What is the difference between a hurricane and a typhoon?",
+    "What are Large Language Models?",
+    "How can you define Artificial Intelligence?",
+]
