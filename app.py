7b6f19288c556f56d30deae11941920d3a1d771a
Christopher <c.goetze@outlook.com>
2024-06-05 17:54:35 +0200

    Added new font

--- app.py
+++ app.py
@@ -78,8 +78,10 @@ def _update_display(questions, answers):
     return out
 
 
+font = "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+
 # Define app
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LITERA, dbc.icons.FONT_AWESOME])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LITERA, dbc.icons.FONT_AWESOME, font])
 # server = app.server
 
 
