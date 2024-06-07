906952dd26dfa5dc8cfb045e76c00abef8b8fec4
Christopher <c.goetze@outlook.com>
2024-06-07 19:33:23 +0200

    Changed function name

--- weather_api.py
+++ weather_api.py
@@ -111,7 +111,7 @@ class OpenWeatherMapAPIWrapper:
 
         return f"{weather_current}\n####\n{weather_forecast}"
 
-    def get_icons(self) -> list[str]:
+    def get_icon_urls(self) -> list[str]:
         icon_ids = [data["weather"][0]["icon"] for data in self.weather["daily"]]
         icon_urls = [f"https://openweathermap.org/img/wn/{icon_id}@4x.png" for icon_id in icon_ids]
         return icon_urls
