fe4396e4a59d9c2d349ff1daaedb67aac7e67158
Christopher <c.goetze@outlook.com>
2024-06-28 22:02:40 +0200

    Adjusted port and host

--- app.py
+++ app.py
@@ -339,6 +339,6 @@ def update_theme(theme):
 
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8050))
-    host = str(os.getenv("HOST", "127.0.0.1"))
+    port = int(os.getenv("PORT", 10000))
+    host = str(os.getenv("HOST", "0.0.0.0"))
     app.run_server(debug=False, port=port, host=host)
